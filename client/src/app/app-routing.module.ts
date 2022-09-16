00eca5508c0b7c2a629fecb7c971fc5e480df7d7
Derek Torres <derek.n.torres@gmail.com>
2022-09-17 00:38:57 +0900

    employee service, routing, template setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+ 
+const routes: Routes = [
+ { path: '', redirectTo: 'employees', pathMatch: 'full' },
+ { path: 'employees', component: EmployeesListComponent },
+];
+ 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
